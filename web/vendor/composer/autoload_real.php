64a9a41c397cee052ed58256e88b780074ff4ad1
Goran.Subic <gsubic@gmail.com>
2021-06-26 13:03:59 +0200

    Image Gallery

--- web/vendor/composer/autoload_real.php
+++ web/vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit4c37a68adeee208e653b46f2bf817f17
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit4c37a68adeee208e653b46f2bf817f17', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit4c37a68adeee208e653b46f2bf817f17', 'loadClassLoader'));
@@ -52,24 +50,6 @@ class ComposerAutoloaderInit4c37a68adeee208e653b46f2bf817f17
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInit4c37a68adeee208e653b46f2bf817f17::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire4c37a68adeee208e653b46f2bf817f17($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequire4c37a68adeee208e653b46f2bf817f17($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
